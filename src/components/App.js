3dade98627cd85c049c427641f4ad0d7d6558e59
Samuel Phua <samuel.phua.de.sheng@gmail.com>
2019-12-19 14:18:21 +0800

    add in firebase ui auth for LoginContainer.

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ class App extends Component {
       if (user) {
         this.setState({ user });
       }
+      // firebase.auth().signOut();
     });
   }
 }
--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -2,107 +2,113 @@ import React, { Component } from "react";
 import Header from "./Header";
 
 class LoginContainer extends Component {
-  state = {
-    email: "",
-    phone: "",
-    error: "",
-    showVerification: false,
-    verificationCode: "",
-  };
-
-  handleEmailChange = (event) => {
-    this.setState({ email: event.target.value });
-  };
-
-  handlePhoneChange = (event) => {
-    this.setState({ phone: event.target.value });
-  };
-
-  handleVerificationCodeChange = (event) => {
-    this.setState({ verificationCode: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-    this.setState({ error: "" });
-    if (this.state.email && this.state.phone) {
-      // try to log them in
-      if (this.state.showVerification === false) {
-        this.sendVerificationCode();
-      } else {
-        this.confirmVerificationCode();
-      }
-    } else {
-      // display error reminding them to fill out all fields
-      this.setState({ error: "Please fill in both fields." });
-    }
-  };
-
-  sendVerificationCode = () => {
-    firebase.auth().signInWithPhoneNumber(this.state.phone, window.recaptchaVerifier).then((confirmationResult) => {
-      // SMS sent. Prompt user to type the code from the message, then sign the user in with confirmationResult.confirm(code).
-      window.confirmationResult = confirmationResult;
-      document.getElementById("input-email").disabled = true;
-      document.getElementById("input-phone").disabled = true;
-      document.getElementById("recaptcha-container").style.display = "none";
-      this.setState({ showVerification: true });
-    }).catch((error) => {
-      console.log("error", error);
-      if (error.code === "auth/user-not-found") {
-        // Sign up here.
-      } else {
-        this.setState({ error: "Error logging in." });
-      }
-      grecaptcha.reset(window.recaptchaWidgetId);
-    });
-  };
-
-  confirmVerificationCode = () => {
-    window.confirmationResult.confirm(this.state.verificationCode).then((result) => {
-      // User signed in successfully.
-      var user = result.user;
-      console.log("login successful", user, result);
-      // ...
-    }).catch((error) => {
-      // User couldn't sign in (bad verification code?)
-      console.log("error", error);
-      // ...
-    });
-  };
-
   render() {
     return (
       <div id="LoginContainer" className="inner-container">
         <Header />
-        <form onSubmit={this.handleSubmit}>
-          <p>Sign in or sign up by entering your email and phone number.</p>
-          <input id="input-email" type="text" pattern="[a-zA-Z0-9-_.]+[@][a-zA-z0-9-_]+[.][a-zA-z]+" value={this.state.email} onChange={this.handleEmailChange} placeholder="Your email" required />
-          <input id="input-phone" type="tel" pattern="[+][0-9]{2}[0-9]{8}" value={this.state.phone} onChange={this.handlePhoneChange} placeholder="Your mobile (e.g. +6598765432)" required />
-          <div id="recaptcha-container"></div>
-          { this.state.showVerification ? <input id="input-verification-code" type="text" value={this.state.verificationCode} onChange={this.handleVerificationCodeChange} placeholder="Verification Code" /> : null }
-          <p className="error">{this.state.error}</p>
-          <button className="red light" type="submit">{ this.state.showVerification ? "Login" : "Get Verification Code" }</button>
-        </form>
+        <div id="loader">Loading ...</div>
+        <div id="firebaseui-auth-container"></div>
       </div>
     );
   }
 
   componentDidMount() {
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier("recaptcha-container", {
-      'size': 'normal',
-      'callback': function(response) {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
-        // ...
-      },
-      'expired-callback': function() {
-        // Response expired. Ask user to solve reCAPTCHA again.
-        // ...
+    const googleSignInOption = {
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      scopes: [
+        "https://www.googleapis.com/auth/contacts.readonly",
+      ],
+      customParameters: {
+        // Forces account selection even when one account
+        // is available.
+        prompt: "select_account"
       }
-    });
-    window.recaptchaVerifier.render().then((widgetId) => {
-      window.recaptchaWidgetId = widgetId;
-    });
+    };
+    const facebookSignInOption = {
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      scopes: [
+        "public_profile",
+        "email",
+        "user_likes",
+        "user_friends"
+      ],
+      customParameters: {
+        // Forces password re-entry.
+        auth_type: "reauthenticate"
+      }
+    };
+    const phoneSignInOption = {
+      provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      recaptchaParameters: {
+        type: "image", // 'audio'
+        size: "normal", // 'invisible' or 'compact'
+        badge: "bottomleft" //' bottomright' or 'inline' applies to invisible.
+      },
+      defaultCountry: "SG", // Set default country to the United Kingdom (+44).
+      // For prefilling the national number, set defaultNationNumber.
+      // This will only be observed if only phone Auth provider is used since
+      // for multiple providers, the NASCAR screen will always render first
+      // with a 'sign in with phone number' button.
+      defaultNationalNumber: "98765432",
+      // You can also pass the full phone number string instead of the
+      // 'defaultCountry' and 'defaultNationalNumber'. However, in this case,
+      // the first country ID that matches the country code will be used to
+      // populate the country selector. So for countries that share the same
+      // country code, the selected country may not be the expected one.
+      // In that case, pass the 'defaultCountry' instead to ensure the exact
+      // country is selected. The 'defaultCountry' and 'defaultNationaNumber'
+      // will always have higher priority than 'loginHint' which will be ignored
+      // in their favor. In this case, the default country will be 'GB' even
+      // though 'loginHint' specified the country code as '+1'.
+      loginHint: "+6598765432"
+    };
+    const emailSignInOption = {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+    };
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          document.getElementById("loader").style.display = "initial";
+          console.log("auth result", authResult);
+          console.log("redirect", redirectUrl);
+          // authResult.operationType === "signIn"
+          // authResult.additionalUserInfo.providerId === "phone"
+          // authResult.additionalUserInfo.isNewUser === false
+          // authResult.user.uid
+          // authResult.user.refreshToken
+          // authResult.user.displayName
+          // authResult.user.email
+          // authResult.user.emailVerified === true
+          // authResult.user.phoneNumber
+          return false;
+        },
+        uiShown: () => {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById("loader").style.display = "none";
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: "popup",
+      signInOptions: [
+        googleSignInOption,
+        // facebookSignInOption,
+        phoneSignInOption,
+        emailSignInOption,
+      ],
+      signInSuccessUrl: "https://www.example.com",
+      // Terms of service url.
+      tosUrl: "https://www.example.com",
+      // Privacy policy url.
+      privacyPolicyUrl: "https://www.example.com",
+    };
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#firebaseui-auth-container", uiConfig);
   }
 }
 
