90fd859c1463d0ea9c1a890eeece5792499561a4
Samuel Phua <samuel.phua.de.sheng@gmail.com>
2020-01-02 16:39:40 +0800

    added in cache control

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,13 +12,42 @@ firebase.initializeApp({
   appId: window.appId,
   measurementId: window.measurementId,
 });
-console.log("firebase messaging", firebase.messaging());
-self.addEventListener("install", function() {
-  console.log("Install!");
-});
-self.addEventListener("activate", function() {
-  console.log("Activate!");
+const CACHE_NAME = "v1";
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      fetch("asset-manifest.json").then(function(response) {
+        if (response.ok) {
+          response.json().then(function(manifest) {
+            const urls = Object.keys(manifest).map(function(key) {
+              return manifest[key];
+            });
+            urls.push("/");
+            urls.push("/assets/icon.png");
+            cache.addAll(urls);
+          });
+        }
+      });
+    }),
+  );
 });
 self.addEventListener("fetch", function(event) {
-  console.log("Fetch!", event.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }),
+  );
+});
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      Promise.all(
+        keyList.map(function(key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
 });
